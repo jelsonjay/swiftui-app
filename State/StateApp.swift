cf4025664ab6b330fb11a6d019df78e3adf8d844
Jelson J <50907905+jelsonjay@users.noreply.github.com>
2023-02-09 09:38:09 +0000

    Initial Commit

---
+++ State/StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateApp.swift
+//  State
+//
+//  Created by Jelson J on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
